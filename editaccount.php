077553e5ceeb5d482e151b16abd5573a453620df
MeanSoNat <nattaphon_wo@kkumail.com>
2022-04-05 00:36:34 +0700

    update
    bio

--- editaccount.php
+++ editaccount.php
@@ -1,3 +1,18 @@
 <?php
-    
+    session_start();
+    $link = mysqli_connect('localhost', 'root', '', 'roitip_db');
+        // get parameters from update
+    $id_user = $_SESSION['id'];
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $bio = $_POST['bio'];
+        // write sql to update profile user
+    $sql = "UPDATE user SET username='$username', email='$email', bio='$bio' WHERE id='$id_user'";
+
+    if(isset($_REQUEST['change_data'])){
+        mysqli_query($link, $sql);
+        header('Location:account.php');
+    } else {
+        exit();
+    }
 ?>
